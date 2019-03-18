a68e9f3f757abb91780874cd569179778fb626b0
thegrinder <paul.szczepkowski@gmail.com>
2019-03-18 19:30:00 +0100

    test(useAccordion): write basic tests for prop getters

--- src/useAccordion/__tests__/useAccordion.js
+++ src/useAccordion/__tests__/useAccordion.js
@@ -2,13 +2,30 @@ import { renderHook, cleanup } from 'react-hooks-testing-library';
 import useAccordion from '../useAccordion';
 
 describe('useAccordion', () => {
+  let hook;
+
+  beforeEach(() => {
+    hook = renderHook(() => useAccordion()).result;
+  });
+
   afterEach(cleanup);
 
   it('should return the correct object', () => {
-    const { result } = renderHook(() => useAccordion());
     ['getHeaderProps', 'getSectionProps', 'isActiveSection'].forEach((property) => {
-      expect(result.current).toHaveProperty(property);
+      expect(hook.current).toHaveProperty(property);
     });
-    expect(result.current).toMatchSnapshot();
+    expect(hook.current).toMatchSnapshot();
+  });
+
+  describe('getHeaderProps', () => {
+    it('should return correct props', () => {
+      expect(hook.current.getHeaderProps('id', 0)).toMatchSnapshot();
+    });
+  });
+
+  describe('getSectionProps', () => {
+    it('should return correct props', () => {
+      expect(hook.current.getSectionProps('id', 0)).toMatchSnapshot();
+    });
   });
 });
