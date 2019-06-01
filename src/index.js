545529aea01eb3f5f3e8fa398b74a1196d352ab2
thegrinder <paul.szczepkowski@gmail.com>
2019-06-01 21:23:01 +0200

    docs(useRequest): write simple explanation and add an example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import useForm from './useForm/useForm';
 import useAccordion from './useAccordion/useAccordion';
+import useRequest from './useRequest/useRequest';
 
 export {
   useForm,
   useAccordion,
+  useRequest,
 };
