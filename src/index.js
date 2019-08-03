1bde7dc410a2515ef0dba31c37e6548a441b99b5
thegrinder <paul.szczepkowski@gmail.com>
2019-08-03 21:39:57 +0200

    feat(usetoggle): add useToggle hook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import useForm from './useForm/useForm';
 import useAccordion from './useAccordion/useAccordion';
 import useRequest from './useRequest/useRequest';
+import useToggle from './useToggle/useToggle';
 
-export { useForm, useAccordion, useRequest };
+export { useForm, useAccordion, useRequest, useToggle };
---
+++ src/useToggle/useToggle.js
@@ -0,0 +1,14 @@
+import { useState, useMemo } from 'react';
+
+const useToggle = (initial = false) => {
+  const [on, set] = useState(initial);
+  const actions = {
+    toggle: () => set(prevState => !prevState),
+    toggleOn: () => set(true),
+    toggleOff: () => set(false),
+  };
+  const value = useMemo(() => [on, actions], [actions, on]);
+  return value;
+};
+
+export default useToggle;
