1929af36f91f45c1a5c07e70abf010020f4f6eff
thegrinder <paul.szczepkowski@gmail.com>
2019-08-03 21:03:14 +0200

    perf(useaccordion): utilize useCallback in useAccordion

--- src/useAccordion/useAccordion.js
+++ src/useAccordion/useAccordion.js
@@ -1,42 +1,53 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useCallback } from 'react';
 import { mod, isArrowDown, isArrowUp } from '../utils';
 
 const useAccordion = () => {
   const headersRefs = useRef([]).current;
 
-  const getHeaderRef = index => node => {
-    if (node === null) {
-      headersRefs.splice(index, 1);
-    } else {
-      headersRefs[index] = node;
-    }
-  };
+  const getHeaderRef = useCallback(
+    index => node => {
+      if (node === null) {
+        headersRefs.splice(index, 1);
+      } else {
+        headersRefs[index] = node;
+      }
+    },
+    [headersRefs]
+  );
 
   const sectionsRefs = useRef([]).current;
 
-  const getSectionRef = index => node => {
-    if (node === null) {
-      sectionsRefs.splice(index, 1);
-    } else {
-      sectionsRefs[index] = node;
-    }
-  };
+  const getSectionRef = useCallback(
+    index => node => {
+      if (node === null) {
+        sectionsRefs.splice(index, 1);
+      } else {
+        sectionsRefs[index] = node;
+      }
+    },
+    [sectionsRefs]
+  );
 
   const [activeSections, updateActiveSections] = useState([]);
 
-  const isActiveSection = index => activeSections.includes(index);
+  const isActiveSection = useCallback(index => activeSections.includes(index), [
+    activeSections,
+  ]);
 
-  const getSectionStyle = index => {
-    const sectionRef = sectionsRefs[index];
-    const baseStyle = {
-      overflow: 'hidden',
-      transition: 'height 200ms linear',
-    };
-    if (!sectionRef || !isActiveSection(index)) {
-      return { ...baseStyle, height: '0px' };
-    }
-    return { ...baseStyle, height: `${sectionRef.clientHeight}px` };
-  };
+  const getSectionStyle = useCallback(
+    index => {
+      const sectionRef = sectionsRefs[index];
+      const baseStyle = {
+        overflow: 'hidden',
+        transition: 'height 200ms linear',
+      };
+      if (!sectionRef || !isActiveSection(index)) {
+        return { ...baseStyle, height: '0px' };
+      }
+      return { ...baseStyle, height: `${sectionRef.clientHeight}px` };
+    },
+    [isActiveSection, sectionsRefs]
+  );
 
   const toggleAccordionSection = index =>
     updateActiveSections(prevSections =>
@@ -47,58 +58,73 @@ const useAccordion = () => {
 
   const [cursorPosition, updateCursorPosition] = useState(-1);
 
-  const getOnClick = index => () => {
-    updateCursorPosition(index);
-    toggleAccordionSection(index);
-  };
+  const getOnClick = useCallback(
+    index => () => {
+      updateCursorPosition(index);
+      toggleAccordionSection(index);
+    },
+    []
+  );
 
-  const focusOnHeader = nextCursorPosition => {
-    const headerRef = headersRefs[nextCursorPosition];
-    if (headerRef) {
-      headerRef.focus();
-    }
-  };
+  const focusOnHeader = useCallback(
+    nextCursorPosition => {
+      const headerRef = headersRefs[nextCursorPosition];
+      if (headerRef) {
+        headerRef.focus();
+      }
+    },
+    [headersRefs]
+  );
 
   const getOnFocus = index => () => updateCursorPosition(index);
 
   const getLinkingId = id => `${id}-tab`;
 
-  const onKeyDown = e => {
-    const { length } = headersRefs;
-    if (isArrowUp(e)) {
-      e.preventDefault();
-      const nextCursorPosition = mod(cursorPosition - 1, length);
-      updateCursorPosition(nextCursorPosition);
-      focusOnHeader(nextCursorPosition);
-    } else if (isArrowDown(e)) {
-      e.preventDefault();
-      const nextCursorPosition = mod(cursorPosition + 1, length);
-      updateCursorPosition(nextCursorPosition);
-      focusOnHeader(nextCursorPosition);
-    }
-  };
+  const onKeyDown = useCallback(
+    e => {
+      const { length } = headersRefs;
+      if (isArrowUp(e)) {
+        e.preventDefault();
+        const nextCursorPosition = mod(cursorPosition - 1, length);
+        updateCursorPosition(nextCursorPosition);
+        focusOnHeader(nextCursorPosition);
+      } else if (isArrowDown(e)) {
+        e.preventDefault();
+        const nextCursorPosition = mod(cursorPosition + 1, length);
+        updateCursorPosition(nextCursorPosition);
+        focusOnHeader(nextCursorPosition);
+      }
+    },
+    [cursorPosition, focusOnHeader, headersRefs]
+  );
 
-  const getHeaderProps = (id, index) => ({
-    'aria-controls': getLinkingId(id),
-    'aria-disabled': isActiveSection(index),
-    'aria-expanded': isActiveSection(index),
-    id,
-    key: id,
-    onClick: getOnClick(index),
-    onFocus: getOnFocus(index),
-    onKeyDown,
-    ref: getHeaderRef(index),
-  });
+  const getHeaderProps = useCallback(
+    (id, index) => ({
+      'aria-controls': getLinkingId(id),
+      'aria-disabled': isActiveSection(index),
+      'aria-expanded': isActiveSection(index),
+      id,
+      key: id,
+      onClick: getOnClick(index),
+      onFocus: getOnFocus(index),
+      onKeyDown,
+      ref: getHeaderRef(index),
+    }),
+    [getHeaderRef, getOnClick, isActiveSection, onKeyDown]
+  );
 
-  const getSectionProps = (id, index) => ({
-    'aria-hidden': !isActiveSection(index),
-    'aria-labelledby': id,
-    id: getLinkingId(id),
-    key: getLinkingId(id),
-    ref: getSectionRef(index),
-    role: 'region',
-    style: getSectionStyle(index),
-  });
+  const getSectionProps = useCallback(
+    (id, index) => ({
+      'aria-hidden': !isActiveSection(index),
+      'aria-labelledby': id,
+      id: getLinkingId(id),
+      key: getLinkingId(id),
+      ref: getSectionRef(index),
+      role: 'region',
+      style: getSectionStyle(index),
+    }),
+    [getSectionRef, getSectionStyle, isActiveSection]
+  );
 
   return {
     getHeaderProps,
