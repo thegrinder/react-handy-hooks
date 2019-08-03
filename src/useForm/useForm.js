2e0acbcd037e3af166aec0e29ace5ac7e20ff102
thegrinder <paul.szczepkowski@gmail.com>
2019-08-03 20:55:04 +0200

    perf(useform): utilize useMemo and useCallback in useForm

--- src/useForm/useForm.js
+++ src/useForm/useForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useCallback, useMemo, useEffect } from 'react';
 import equal from 'fast-deep-equal';
 
 const useForm = ({ initialValues, validate, onSubmit, submitting = false }) => {
@@ -6,47 +6,59 @@ const useForm = ({ initialValues, validate, onSubmit, submitting = false }) => {
   const [errors, updateErrors] = useState({});
   const [touched, updateTouched] = useState({});
 
-  const runValidation = formValues => {
-    if (validate) {
-      updateErrors(validate(formValues));
-    }
-  };
+  const runValidation = useCallback(
+    formValues => {
+      if (validate) {
+        updateErrors(validate(formValues));
+      }
+    },
+    [validate]
+  );
 
   useEffect(() => {
     runValidation(values);
-  }, []);
+  }, [runValidation, values]);
 
-  const invalid = !!Object.keys(errors).length;
+  const invalid = useMemo(() => !!Object.keys(errors).length, [errors]);
 
-  const pristine = equal(initialValues, values);
+  const pristine = useMemo(() => equal(initialValues, values), [
+    initialValues,
+    values,
+  ]);
 
-  const getFieldProps = name => ({
-    submitting,
-    error: errors[name],
-    touched: !!touched[name],
-    value: values[name],
-    onChange(e) {
-      const newValues = { ...values, [name]: e.target.value };
-      updateValues(newValues);
-      runValidation(newValues);
+  const getFieldProps = useCallback(
+    name => ({
+      submitting,
+      error: errors[name],
+      touched: !!touched[name],
+      value: values[name],
+      onChange(e) {
+        const newValues = { ...values, [name]: e.target.value };
+        updateValues(newValues);
+        runValidation(newValues);
+      },
+      onBlur() {
+        updateTouched({ ...touched, [name]: true });
+      },
+    }),
+    [errors, runValidation, submitting, touched, values]
+  );
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      if (!invalid && onSubmit) {
+        onSubmit(values);
+      }
     },
-    onBlur() {
-      updateTouched({ ...touched, [name]: true });
-    },
-  });
+    [invalid, onSubmit, values]
+  );
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!invalid && onSubmit) {
-      onSubmit(values);
-    }
-  }
-
-  const resetForm = () => {
+  const resetForm = useCallback(() => {
     updateValues(initialValues);
     updateErrors({});
     updateTouched({});
-  };
+  }, [initialValues]);
 
   return {
     getFieldProps,
